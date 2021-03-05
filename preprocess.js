64160cbf4fc56a55a7814a91bfae51b2ec58eaf6
Stephen Buchanan <stephen.buchanan@random.studio>
2021-03-05 13:21:59 +0100

    list files in directory async

--- preprocess.js
+++ preprocess.js
@@ -1,3 +1,16 @@
+const fs = require('fs/promises');
+const path = require('path');
+
 const sharp = require('sharp');
 
-// await 
+const main = async() => {
+  const files = await fs.readdir(path.resolve('./originals/unzipped'));
+  console.log(files.length);
+}
+
+
+main().then(res => {
+
+}).catch(e =>
+  console.error(e)
+)

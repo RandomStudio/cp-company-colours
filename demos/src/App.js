1a007b8c279df1ddea42a4128b1aeea1065c0562
Stephen Buchanan <stephen.buchanan@random.studio>
2021-03-08 10:01:34 +0100

    react with typescript

---
+++ demos/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.scss";
+import SimpleSort from "./SimpleSort";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Colours</header>
+      <SimpleSort />
+    </div>
+  );
+}
+
+export default App;
---
+++ demos/src/SimpleSort.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as PIXI from "pixi.js";
+
+interface Props {}
+
+export const SimpleSort: React.FunctionComponent<Props> = (props: Props) => {
+  return <div>Boo!</div>;
+};
+
+export default SimpleSort;
